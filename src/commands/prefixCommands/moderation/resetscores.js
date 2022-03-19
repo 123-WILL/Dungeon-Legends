d0f6e3ff1bc171ab65aad5aa673e9e1a4850ba91
123-WILL <william.sugden@outlook.com>
2022-03-19 10:32:22 -0700

    added resetscores and started working on apply command

---
+++ src/commands/prefixCommands/moderation/resetscores.js
@@ -0,0 +1,37 @@
+const carrierModel = require("../../../schemas/carrier");
+
+module.exports = {
+    name: 'resetscores',
+    description: 'resets scores.',
+    async execute(message, client, args) {
+        if (!process.env.owners.includes(message.author.id)) return;
+
+        const carrierRoleIds = process.env.carrier_role_ids.split(', ');
+        try {
+            await message.guild.members.fetch();
+
+            carrierRoleIds.forEach(carrierRoleID => {
+                const carriersWithRole = message.guild.roles.cache.get(carrierRoleID).members;
+                carriersWithRole.forEach(carrier => {
+                    client.carriers.set(carrier.id, { carrierScore: 0 })
+                    let query = { discordID: carrier.id };
+                    carrierModel.findOneAndUpdate(query, { carrierScore: 0 }).catch((err) => console.log(err));
+                })
+            })
+        } catch (err) {
+            console.error(err);
+        }
+
+
+        const embed = {
+            title: `**Success.**`,
+            description: "All scores have been reset.",
+            color: 7506394,
+            footer: {
+                text: `Dungeon Legends`,
+                icon_url: "https://cdn.discordapp.com/attachments/827662473880535042/827913817064734801/standard_14.gif"
+            }
+        }
+        await message.reply({ embeds: [embed] });
+    },
+};
